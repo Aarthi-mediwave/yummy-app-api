dd21de2ae216c3af9eaeef00d42d004a6d259e15
Aarthikootharasan <91037876+Aarthikootharasan@users.noreply.github.com>
2023-09-01 12:20:28 +0530

    POST request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.use(express.json());
 const recipes = [
     {
         id:101,
@@ -32,9 +33,22 @@ app.put('/',(req,res) =>{
     res.send("PUT request")
 })
 
-app.post('/',(req,res) =>{
-    res.send("POST request")
+//adding a receipe
+app.post('/recipes',(req,res) =>{
+    const payload = req.body;
+    if(!payload.name) {
+        return res.status(400).send({message:"Receipe should have a name"});
+    }
+    payload.id=new Date().getTime();
+    recipes.push(payload);
+     return res.status(201).send(payload);
 })
+
+app.use((req, res, next) => {
+    return res.status(404).json({
+        message:"Resource not found",
+    })
+});
 
 app.delete('/',(req,res) =>{
     res.send("DELETE request")
